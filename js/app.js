f0128f13ed17286bbc8f6706fc9d68f9cd9eab14
Jacob Mink <jacobmink@gmail.com>
2018-12-16 20:21:25 -0700

    data structures

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 // DATA STRUCTURES
 
+// Data Types
 // light switch: boolean -> const lightSwitch = true;
 // email address: string -> 'fake@nothing.com';
 // spaceship: object -> const ship = {body: hull, weapons: laser blasters, tools: [tractor beam, warp drive]};
@@ -9,3 +10,13 @@
 // list of students with locations and list of favorite tv shows: array of objects with an array inside each -> 
 //  const hugeList = [{name: 'student1',location: 'Alabama', fave_shows: [tvShow1,tvShow2]},
 //                      {name: 'student2',location: 'New York', fave_shows: [tvShow1,tvShow2]}];
+
+// Take it Easy
+// 1)
+const colorArr = ['red','orange','yellow','green','blue','indigo','violet'];
+// 2)
+console.log(colorArr[colorArr.indexOf('blue')]);
+// 3)
+const jake = {favoriteFood: 'noodles', hobby: 'skyweaving', townName: 'Denver', faveData: 'object'};
+// 4)
+console.log(jake.hobby);
