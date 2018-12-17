5e3342675db9f36c1e4d2708df1e2b02a34ab43c
Jacob Mink <jacobmink@gmail.com>
2018-12-16 20:16:45 -0700

    data types

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// DATA STRUCTURES
+
+// light switch: boolean -> const lightSwitch = true;
+// email address: string -> 'fake@nothing.com';
+// spaceship: object -> const ship = {body: hull, weapons: laser blasters, tools: [tractor beam, warp drive]};
+// list of student names: array of strings -> const nameList = ['student1','student2',etc.];
+// list of students with locations: array of objects -> const bigList = [{name: 'student1',location: 'Alabama'},
+//                                                                      {name: 'student2',location: 'New York'}];
+// list of students with locations and list of favorite tv shows: array of objects with an array inside each -> 
+//  const hugeList = [{name: 'student1',location: 'Alabama', fave_shows: [tvShow1,tvShow2]},
+//                      {name: 'student2',location: 'New York', fave_shows: [tvShow1,tvShow2]}];
